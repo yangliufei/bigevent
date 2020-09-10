8a70560ff541b932c8b7a57dd3a02f1d71eea052
lfy <2194558064@qq.com>
2020-09-10 11:46:08 +0800

    完善登录页面

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,13 +1,14 @@
 $(function () {
-    $('#tologin').on('click', function () {
+    $('#toreg').on('click', function () {
         $('.login_box').hide()
         $('.reg_box').show()
     })
-    $('#toreg').on('click', function () {
+    $('#tologin').on('click', function () {
         $('.login_box').show()
         $('.reg_box').hide()
     })
     var form = layui.form
+    var layer = layui.layer
     form.verify({
         psd: [
             /^[\S]{6,12}$/
@@ -20,5 +21,41 @@ $(function () {
             }
         }
 
+    })
+    $('#form_reg').on("submit", function (e) {
+        e.preventDefault()
+        $.ajax({
+            url: "http://ajax.frontend.itheima.net/api/reguser",
+            type: 'post',
+            data: {
+                username: $("#form_reg [name=username]").val(),
+                password: $("#form_reg [name=password]").val()
+            },
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg(res.message);
+                }
+                layer.msg('注册成功,请登录');
+                $('#tologin').click()
+            }
+        })
+    })
+    $('#login_form').submit(function(e){
+        e.preventDefault()
+        $.ajax({
+            url:"http://ajax.frontend.itheima.net/api/login",
+            type:'post',
+            data:$(this).serialize(),
+            success:function(res){
+                if(res.status !==0){
+                    return layer.msg(res.message)
+                }
+                layer.msg('登录成功')
+
+                localStorage.setItem('token',res.token)
+                location.href='/index.html'
+                
+            }
+        })
     })
 })
