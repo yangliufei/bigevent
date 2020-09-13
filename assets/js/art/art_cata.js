27ffc779c85bcd8aae991e980acc984f84fd819f
lfy <2194558064@qq.com>
2020-09-13 15:58:57 +0800

    文章类别完成

--- assets/js/art/art_cata.js
+++ assets/js/art/art_cata.js
@@ -38,4 +38,61 @@ $(function () {
       }
     })
   })
+  var editIndex = null
+  $('body').on('click', '.btn-edit', function () {
+    editIndex = layui.layer.open({
+      type: 1,
+      area: ['400px', '250px'],
+      title: '修改文章分类',
+      content: $('#dailog-edit').html()
+    })
+    var cataid = $(this).attr('data-id')
+    $.ajax({
+      type: 'get',
+      url: `/my/article/cates/${cataid}`,
+      success: function (res) {
+        if (res.status !== 0) {
+          return layui.layer.msg(res.message)
+        }
+        layui.form.val('edit-form', res.data)
+      }
+    })
+  })
+
+  $('body').on('submit', '#form-edit', function (e) {
+    e.preventDefault()
+    $.ajax({
+      type: 'post',
+      url: '/my/article/updatecate',
+      data: $(this).serialize(),
+      success: function (res) {
+        if (res.status !== 0) {
+          return layui.layer.msg(res.message)
+        }
+        layui.layer.msg('修改分类成功')
+        xrcatalist()
+        layui.layer.close(editIndex)
+
+      }
+    })
+  })
+  $('tbody').on('click', '.btn-delete', function () {
+    var cataid = $(this).attr('data-id')
+    layer.confirm('是否确认删除?', { icon: 3, title: '提示' }, function (index) {
+      $.ajax({
+        type: 'get',
+        url: `/my/article/deletecate/${cataid}`,
+        success: function (res) {
+          if (res.status !== 0) {
+            return layui.layer.msg(res.message)
+          }
+          layui.layer.msg('删除成功')
+          xrcatalist()
+          layui.layer.close()
+
+        }
+
+      })
+    })
+  })
 })
